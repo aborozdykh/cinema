695f9f8986d6bbc1e265722954727d428b6d5bd0
Andrii Borozdykh <aborozdykh@gmail.com>
2020-05-20 16:58:13 +0300

    Create DAO for Movie, create methods getAll(), add()

--- src/main/java/com/dev/cinema/Main.java
+++ src/main/java/com/dev/cinema/Main.java
@@ -5,12 +5,15 @@ import com.dev.cinema.models.Movie;
 import com.dev.cinema.service.MovieService;
 
 public class Main {
-    private static Injector injector = Injector.getInstance("<YOUR_PACKAGE>");
+    private static Injector injector = Injector.getInstance("com.dev.cinema");
 
     public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        movieService.getAll().forEach(System.out::println);
+
         Movie movie = new Movie();
         movie.setTitle("Fast and Furious");
-        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        movie.setDescription("The best of the best");
         movieService.add(movie);
 
         movieService.getAll().forEach(System.out::println);
--- src/main/java/com/dev/cinema/dao/impl/MovieDaoImpl.java
+++ src/main/java/com/dev/cinema/dao/impl/MovieDaoImpl.java
@@ -1,10 +1,47 @@
 package com.dev.cinema.dao.impl;
 
+import java.util.List;
+import javax.persistence.criteria.CriteriaQuery;
+import com.dev.cinema.dao.MovieDao;
+import com.dev.cinema.exceptions.DataProcessingException;
 import com.dev.cinema.lib.Dao;
+import com.dev.cinema.models.Movie;
+import com.dev.cinema.util.HibernateUtil;
 import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 
 @Dao
-public class MovieDaoImpl {
+public class MovieDaoImpl implements MovieDao {
     private static final Logger LOGGER = Logger.getLogger(MovieDaoImpl.class);
 
+    @Override
+    public Movie add(Movie movie) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Long itemId = (Long) session.save(movie);
+            transaction.commit();
+            movie.setId(itemId);
+            return movie;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't add Movie entity", e);
+        }
+    }
+
+    @Override
+    public List<Movie> getAll() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            CriteriaQuery<Movie> criteriaQuery = session.getCriteriaBuilder()
+                    .createQuery(Movie.class);
+            criteriaQuery.from(Movie.class);
+            return session.createQuery(criteriaQuery).getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get list of all movies ", e);
+        }
+    }
 }
