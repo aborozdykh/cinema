46362155a1a5f534b8eac30ac254c4c2ae4b475d
Andrii Borozdykh <aborozdykh@gmail.com>
2020-06-13 21:27:32 +0300

    Replaced lambda with a method reference in CustomGlobalExceptionHandler

--- src/main/java/me/aborozdykh/cinema/controller/CustomGlobalExceptionHandler.java
+++ src/main/java/me/aborozdykh/cinema/controller/CustomGlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,7 +31,7 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
         List<String> errors = ex.getBindingResult()
                 .getFieldErrors()
                 .stream()
-                .map(x -> x.getDefaultMessage())
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .collect(Collectors.toList());
         body.put("errors", errors);
         return new ResponseEntity<>(body, headers, status);
